fdf58801c2eac3d48f3e7a23d98c225b0d3f7b86
devchosil <101618759+devchosil@users.noreply.github.com>
2022-08-01 22:06:34 +0900

    test

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.use(express.urlencoded({extended : true}));
-app.set('view engine', 'ejs');
 require('dotenv').config();
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB 접속 쉽게 도와주는 라이브러리 require
 const MongoClient = require('mongodb').MongoClient;
@@ -27,6 +29,7 @@ app.use(session({secret : '비밀코드', resave : true, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session()); 
 
+//.아이디, 비번 인증(DB와 맞는지 비교)
 passport.use(new LocalStrategy({
   usernameField: 'id',
   passwordField: 'pw',
@@ -50,15 +53,56 @@ app.get('/', (요청, 응답)=>{
   응답.render('index.ejs');
 })
 
-app.post('/', passport.authenticate('local', {failureMessage : '아이디와 비밀번호를 다시 확인해주세요'}), function(요청, 응답){
-  응답.redirect('/');
+app.post('/', passport.authenticate('local', {failureRedirect: '/', failureFlash: true}), function(요청, 응답){
+  응답.redirect('/list');
   console.log('로그인 성공');
 });
 
+//세션만들고 세션아이디 발급해서 쿠키로 보내주기
 passport.serializeUser(function (user, done) {
   done(null, user.id)
 });
 
 passport.deserializeUser(function (아이디, done) {
-  done(null, {})
-}); 
+  db.collection('login').findOne({ id: 아이디 }, function (에러, 결과) {
+    done(null, 결과)
+  })
+});
+
+//로그인하면 list페이지로 라우팅
+function login(요청, 응답, next) {
+  if(요청.user) {
+    next()
+  } else {
+    응답.send('<script>alert("로그인 해주세요")</script>');
+  }
+}
+
+//로그인 여부 확인후, 페이지 불러오기(user정보도 같이 보내줌)
+app.get('/list', login, (요청, 응답)=>{
+  db.collection('post').find().toArray(function(에러, 결과) {
+    응답.render('list.ejs', { posts: 결과, 사용자 : 요청.user });
+  }); 
+})
+
+app.get('/detail', login, (요청, 응답)=>{
+  응답.render('detail.ejs', {사용자 : 요청.user});
+})
+
+app.get('/update', login, (요청, 응답)=>{
+  응답.render('update.ejs', {사용자 : 요청.user});
+})
+
+app.get('/write', login, (요청, 응답)=>{
+  응답.render('write.ejs', {사용자 : 요청.user});
+})
+
+app.post('/write', (요청, 응답) =>{
+  응답.render('write.ejs');
+  var 저장할거 =  { 제목 : 요청.body.title, 내용 : 요청.body.content, 작성자: 요청.user._id}
+
+  //새로운 데이터를 post컬렉션에 저장
+  db.collection('post').insertOne(저장할거, function(에러, 결과){
+    console.log('저장완료')
+  });
+})
